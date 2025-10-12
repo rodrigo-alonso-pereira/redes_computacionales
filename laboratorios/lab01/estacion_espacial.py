c1466818b52fdfd148b77211d85ab651045d6efc
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2025-10-12 17:10:03 -0300

    ADD: Sistema para comunicaciones de estacion espacial

---
+++ laboratorios/lab01/estacion_espacial.py
@@ -0,0 +1,52 @@
+import socket
+
+HOST = '127.0.0.1'
+
+tcp_port = 8001
+udp_port = 8002
+
+def verificar_mensaje_tcp(mensaje):
+    if mensaje.startswith("REPORTE:") or mensaje == "CONSULTAR" or mensaje == "MISION_COMPLETA":
+        return True
+    return False
+
+while True:
+    print("\n[ESTACION ESPACIAL] Iniciando sistema de comunicaciones...")
+    print("1. Conectar con Centro de Control")
+    print("2. Enviar alerta rapida")
+    print("3. Abortar el envio de mensajes y salir")
+    opcion = input("Seleccione una opcion (1-3): ")
+
+    if opcion == '1': # Conexion Centro de Control [TCP]
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente_tcp:
+            cliente_tcp.connect((HOST, tcp_port))
+            print("[ESTACION ESPACIAL] Conectado con Centro de Control (TCP)")
+            mensaje = ""
+            while mensaje != "MISION_COMPLETA":
+                mensaje = input("Astronauta> ")
+                if not verificar_mensaje_tcp(mensaje):
+                    print("Mensaje no permitido. Intente de nuevo con 'REPORTE:', 'CONSULTAR', 'MISION_COMPLETA'.")
+                    continue
+                cliente_tcp.sendall((mensaje + "\n").encode('utf-8'))
+                print("Centro de control: ", cliente_tcp.recv(1024).decode('utf-8'))
+            print("[ESTACION ESPACIAL] Desconectado de Centro de Control")
+        break
+    elif opcion == '2':
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as cliente_udp:
+            print("[ESTACION ESPACIAL] Conectado con Sistema de Alertas (UDP)")
+            mensaje = ""
+            while mensaje != "base_segura":
+                mensaje = input("Emergencia> ")
+                cliente_udp.sendto(mensaje.encode('utf-8'), (HOST, udp_port))
+                if mensaje == "base_segura":
+                    print("Sistema: ", cliente_udp.recv(1024).decode('utf-8'))
+                    break
+                print("Alerta: ", cliente_udp.recv(1024).decode('utf-8'))
+            print("\n[ESTACION ESPACIAL] Desconectado de Sistemas de Alertas")
+        break
+    elif opcion == '3':
+        print("\n[ESTACION ESPACIAL] Saliendo del sistema de comunicaciones. Hasta luego astronauta!")
+        break
+    else:
+        print("Opcion no valida. Intente de nuevo.")
+        continue
